be10ced1997697e266e958be7db12d9f29e74d5b
Bradley <bshellnu@pop-os.localdomain>
2020-09-16 14:36:48 -0700

    Using hyper terminal on PopOS and Ubuntu. Configuring terminal window.

--- .hyper.js
+++ .hyper.js
@@ -1,23 +1,66 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
 module.exports = {
-  updateChannel: 'canary',
   config: {
+    // choose either `'stable'` for receiving highly polished,
+    // or `'canary'` for less polished but more frequent updates
+    updateChannel: 'canary',
+    
     // default font size in pixels for all tabs
-    fontSize: 20,
+    fontSize: 14,
+    
     windowSize: [1080, 720],
+
+    // font family with optional fallbacks
     fontFamily: '"Operator Mono", "Inconsolata for Powerline", monospace',
+    
+    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
     cursorShape: 'BLOCK',
-    wickedBorder: true,
+
+    // set to `true` (without backticks and without quotes) for blinking cursor
+    cursorBlink: true,
+
+    // custom padding (CSS format, i.e.: `top right bottom left`)
     padding: '10px',
+    
     shell: '/bin/zsh',
+
+    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
+    copyOnSelect: `true`,
+
+    // Whether to use the WebGL renderer. Set it to false to use canvas-based
+    // rendering (slower, but supports transparent backgrounds)
+    webGLRenderer: true,
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+    // omit or set true to show. set false to remove it
+    wickedBorder: false,
+
+    // change the colour here
+    wickedBorderColor: '#ffc600',
   },
 
-
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
   plugins: [
     'hyperterm-cobalt2-theme',
+    'hyper-statusline',
+    'hyper-tabs-enhanced'
   ],
 
   // in development, you can create a directory under
-  // `~/.hyperterm_plugins/local/` and include it here
+  // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  // localPlugins: ['hyperterm-cobalt2-theme'],
+  localPlugins: [],
+
+  keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+  },
 };
