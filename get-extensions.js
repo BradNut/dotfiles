687a10be4e486195510a2355f7998f702f313944
Bradley Shellnut <bradleyshellnut@pm.me>
2022-12-20 10:30:22 -0800

    Updating the WesBos extensions script and updating my extensions.

--- get-extensions.js
+++ get-extensions.js
@@ -5,17 +5,48 @@ To get a list of extensions:
 2. type in `@enabled`
 3. Open VS Code dev toosls `Help` → `Toggle Developer Tools`
 4. Paste the code from [get-extensions.js](./get-extensions.js) into the console
-5. It will be in your clipboard, so just paste into `vs-code-extensions-i-use.md`
+5. You have 10 seconds to scroll your extension list to gather them all. This is weird, but VS code uses virtual scrolling so they are only added to the dom when visible.
+5. run copy(markdown), and paste into `vs-code-extensions-i-use.md`
 
 */
 
-function getExtensions() {
-  const extensions = $$('.extensions-list .monaco-list-row').map(function(row) {
+async function getExtensions() {
+  // VS Code uses Virtual scrolling, so we need to get them all
+  // I cannot find where in memory, or how to query a list of @enabled
+  // This is janky AF dont judge me
+
+  // zoom out
+  vscode.webFrame.setZoomLevel(-10);
+  const seconds = 5;
+  console.log(`You have ${seconds} seconds to scroll your list up and down as much as possible!`);
+  const allExtensions = [];
+
+  const interval = setInterval(function() {
+    allExtensions.push(...getVisibleExtensions());
+    console.log('KEEP SCROLLING!');
+  }, 300);
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('PHEW DONE!');
+      clearInterval(interval);
+      vscode.webFrame.setZoomLevel(-1);
+      console.log(allExtensions.length, 'captured');
+      const unique = uniqueBy(allExtensions, 'id');
+      console.log(unique.length, 'unique');
+      resolve(unique)
+    }, seconds * 1000);
+  });
+
+}
+
+function getVisibleExtensions() {
+  const extensions = Array.from(document.querySelectorAll('.extensions-list .monaco-list-row')).map(function (row) {
     let icon = row.querySelector('.icon').src;
     const title = row.querySelector('span.name').textContent;
     const description = row.querySelector('.description').textContent;
     const id = row.dataset.extensionId;
-    if(icon.startsWith('vscode-file')) {
+    if (icon.startsWith('vscode-file')) {
       icon = `https://cdn.vsassets.io/v/M213_20221206.3/_content/Header/default_icon_128.png`;
     }
 
@@ -47,6 +78,22 @@ ${rows.join(`\n`)}
   return markdownTable;
 }
 
-const extensions = getExtensions();
+function uniqueBy(arr, key) {
+  let seen = new Set()
+
+  return arr.filter(it => {
+    let val = it[key]
+    if (seen.has(val)) {
+      return false
+    } else {
+      seen.add(val)
+      return true
+    }
+  })
+}
+
+
+const extensions = await getExtensions();
+console.info(extensions.length, 'Found!');
 const markdown = makeMarkdownTable(extensions);
-copy(markdown);
+console.info('please run copy(markdown) to get the markdown in your clipboard')
