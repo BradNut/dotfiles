9253ebb368a91091ad71ec140f568c0d9b79baeb
Bradley Shellnut <bradleyshellnut@protonmail.com>
2024-09-19 10:28:29 -0700

    Updating plugins, settings, etc.

--- get-extensions.js
+++ get-extensions.js
@@ -3,7 +3,7 @@ To get a list of extensions:
 
 1. Open up your extensions list. `⌘+⇧+x`
 2. type in `@enabled`
-3. Open VS Code dev toosls `Help` → `Toggle Developer Tools`
+3. Open VS Code dev tools `Help` → `Toggle Developer Tools`
 4. Paste the code from [get-extensions.js](./get-extensions.js) into the console
 5. You have 10 seconds to scroll your extension list to gather them all. This is weird, but VS code uses virtual scrolling so they are only added to the dom when visible.
 5. run copy(markdown), and paste into `vs-code-extensions-i-use.md`
@@ -18,38 +18,35 @@ async function getExtensions() {
 	// zoom out
 	vscode.webFrame.setZoomLevel(-10);
 	const seconds = 5;
-	console.log(
-		`You have ${seconds} seconds to scroll your list up and down as much as possible!`
-	);
+	console.log(`You have ${seconds} seconds to scroll your list up and down as much as possible!`);
 	const allExtensions = [];
 
-	const interval = setInterval(function () {
+	const interval = setInterval(function() {
 		allExtensions.push(...getVisibleExtensions());
-		console.log("KEEP SCROLLING!");
+		console.log('KEEP SCROLLING!');
 	}, 300);
 
 	return new Promise((resolve) => {
 		setTimeout(() => {
-			console.log("PHEW DONE!");
+			console.log('PHEW DONE!');
 			clearInterval(interval);
 			vscode.webFrame.setZoomLevel(-1);
-			console.log(allExtensions.length, "captured");
-			const unique = uniqueBy(allExtensions, "id");
-			console.log(unique.length, "unique");
-			resolve(unique);
+			console.log(allExtensions.length, 'captured');
+			const unique = uniqueBy(allExtensions, 'id');
+			console.log(unique.length, 'unique');
+			resolve(unique)
 		}, seconds * 1000);
 	});
+
 }
 
 function getVisibleExtensions() {
-	const extensions = Array.from(
-		document.querySelectorAll(".extensions-list .monaco-list-row")
-	).map(function (row) {
-		let icon = row.querySelector(".icon").src;
-		const title = row.querySelector("span.name").textContent;
-		const description = row.querySelector(".description").textContent;
+	const extensions = Array.from(document.querySelectorAll('.extensions-list .monaco-list-row')).map(function (row) {
+		let icon = row.querySelector('.icon').src;
+		const title = row.querySelector('span.name').textContent;
+		const description = row.querySelector('.description').textContent;
 		const id = row.dataset.extensionId;
-		if (icon.startsWith("vscode-file")) {
+		if (icon.startsWith('vscode-file')) {
 			icon = `https://cdn.vsassets.io/v/M213_20221206.3/_content/Header/default_icon_128.png`;
 		}
 
@@ -57,49 +54,46 @@ function getVisibleExtensions() {
 			icon,
 			title,
 			description,
-			id,
-		};
+			id
+		}
 	});
 	return extensions;
 }
 
 function makeLink(id) {
-	return `https://marketplace.visualstudio.com/items?itemName=${id}`;
+	return `https://marketplace.visualstudio.com/items?itemName=${id}`
 }
 
 function makeMarkdownTable(extensions) {
-	const rows = extensions.map((extension) => {
-		return `| <a href="${makeLink(extension.id)}"><img width="100" src="${
-			extension.icon
-		}" alt="${extension.title}"> | <h3><a href="${makeLink(extension.id)}">${
-			extension.title
-		}</a></h3>${extension.description} |`;
+	const rows = extensions.map(extension => {
+		return `| <a href="${makeLink(extension.id)}"><img width="100" src="${extension.icon}" alt="${extension.title}"> | <h3><a href="${makeLink(extension.id)}">${extension.title}</a></h3>${extension.description} |`
 	});
 
-	const markdownTable = `Here are a list of all the extensions I use. This is an ever-changing setup, so if you saw me use something in a video or screenshot, it might be in this list!
+	const markdownTable =  `Here are a list of all the extensions I use. This is an ever-changing setup, so if you saw me use something in a video or screenshot, it might be in this list!
 
 | ×××××× |  |
 | --- | --- |
 ${rows.join(`\n`)}
-`;
+`
 	return markdownTable;
 }
 
 function uniqueBy(arr, key) {
-	let seen = new Set();
+	let seen = new Set()
 
-	return arr.filter((it) => {
-		let val = it[key];
+	return arr.filter(it => {
+		let val = it[key]
 		if (seen.has(val)) {
-			return false;
+			return false
 		} else {
-			seen.add(val);
-			return true;
+			seen.add(val)
+			return true
 		}
-	});
+	})
 }
 
+
 const extensions = await getExtensions();
-console.info(extensions.length, "Found!");
+console.info(extensions.length, 'Found!');
 const markdown = makeMarkdownTable(extensions);
-console.info("please run copy(markdown) to get the markdown in your clipboard");
+console.info('please run copy(markdown) to get the markdown in your clipboard')
--- intellij-plugins.md
+++ intellij-plugins.md
@@ -1,32 +1,45 @@
-# List of all the plugins I use.
+# IntelliJ Plugins
+
+List of all the plugins I use.
 
 ## Code Editing
 
 - [Codeium](https://plugins.jetbrains.com/plugin/20540-codeium)
+- [Prettier](https://plugins.jetbrains.com/plugin/10456-prettier)
 
 ## Code Tools
 
 - [Rainbow Brackets](https://plugins.jetbrains.com/plugin/10080-rainbow-brackets)
 - [JPA Buddy](https://plugins.jetbrains.com/plugin/15075-jpa-buddy)
+- [Save Actions](https://plugins.jetbrains.com/plugin/7642-save-actions)
+- [shadcn/ui Components Manager](https://plugins.jetbrains.com/plugin/23479-shadcn-ui-components-manager)
 
 ## Code Quality
 
 - [CheckStyle-IDEA](https://plugins.jetbrains.com/plugin/1065-checkstyle-idea)
 - [SonarLint](https://plugins.jetbrains.com/plugin/7973-sonarlint)
+- [Lombok](https://plugins.jetbrains.com/plugin/6317-lombok)
 
 ## Framework
 
+- [VirtualKit](https://plugins.jetbrains.com/plugin/12376-virtualkit)
 - [Svelte](https://plugins.jetbrains.com/plugin/12375-svelte)
+- [Kotlin](https://plugins.jetbrains.com/plugin/6954-kotlin)
 
 ## Productivity
 
 - [GitToolBox](https://plugins.jetbrains.com/plugin/7499-gittoolbox)
+- [Biome](https://plugins.jetbrains.com/plugin/22761-biome)
+- [Conventional Commit](https://plugins.jetbrains.com/plugin/13389-conventional-commit)
 
 ## Theme
 
 - [Material Theme UI](https://plugins.jetbrains.com/plugin/8006-material-theme-ui)
+  - Using Material Darker
+- [One Dark Theme](https://plugins.jetbrains.com/plugin/11938-one-dark-theme)
 - [Visual Studio Code Dark Plus Theme](https://plugins.jetbrains.com/plugin/12255-visual-studio-code-dark-plus-theme)
 
 ## Tools Integration
 
 - [Docker](https://plugins.jetbrains.com/plugin/7724-docker)
+- [.env files](https://plugins.jetbrains.com/plugin/9525--env-files-support)
